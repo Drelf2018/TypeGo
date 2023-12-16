b0a6a8c6c0f965563d2bfd68102074fa64f5d774
Drelf2018 <drelf2018@outlook.com>
2023-12-17 02:37:01 +0800

    feat: Value

---
+++ Reflect/value.go
@@ -0,0 +1,31 @@
+package Reflect
+
+import "reflect"
+
+type Value struct {
+	reflect.Value
+}
+
+func (v Value) Call(in ...reflect.Value) []reflect.Value {
+	return v.Value.Call(in)
+}
+
+func (v Value) CallSlice(in ...reflect.Value) []reflect.Value {
+	return v.Value.CallSlice(in)
+}
+
+func (v Value) CallAny(in ...any) []reflect.Value {
+	return v.Value.Call(ValuesOf(in...))
+}
+
+func (v Value) CallAnySlice(in ...any) []reflect.Value {
+	return v.Value.CallSlice(ValuesOf(in...))
+}
+
+func ValuesOf(in ...any) []reflect.Value {
+	val := make([]reflect.Value, 0, len(in))
+	for _, i := range in {
+		val = append(val, reflect.ValueOf(i))
+	}
+	return val
+}
